9e941c8a795e1852b4a96726037d292abd2dd199
Akram Dghais <115045927+1kram9dghs@users.noreply.github.com>
2023-11-15 23:34:07 +0100

    Add files via upload

---
+++ src/app/update-produit/update-produit.component.ts
@@ -0,0 +1,40 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ProduitService } from '../services/produit.service';
+import { Produit } from '../model/produit.model';
+import { Router } from '@angular/router';
+import { Categorie } from '../model/categorie.model';
+
+
+@Component({
+  selector: 'app-update-produit',
+  templateUrl: './update-produit.component.html',
+  styles: [
+  ]
+})
+export class UpdateProduitComponent implements OnInit {
+  currentProduit: Produit = new Produit();
+  categories! : Categorie[];
+updatedCatId! : number;
+
+  constructor(private activatedRoute: ActivatedRoute,
+              private router: Router,
+              private produitservise: ProduitService) { }
+
+  ngOnInit(): void {
+    //console.log(this.route.snapshot.params.id);
+    this.categories = this.produitservise.listeCategories();
+
+  this.currentProduit = this.produitservise.consulterProduit(this.activatedRoute.snapshot.params['id']);
+  this.updatedCatId=this.currentProduit.categorie.idCat;
+
+  //console.log(this.currentProduit);
+  }
+  updateProduit()
+  { //console.log(this.currentProduit);
+    this.currentProduit.categorie=this.produitservise.consulterCategorie(this.updatedCatId);
+    this.produitservise.updateProduit(this.currentProduit);
+    this.router.navigate(['produits']);
+    
+  }
+}
